20879b60a0b424a8f3cfc464a1726ae0fee34757
Heorhii Savostikov <georgesavostikov@gmail.com>
2026-03-28 22:37:24 +0200

    Created some tests for the function

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,9 +1,27 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {});
+  it('should ', () => {
+    const condition = () => true;
+    const first = jest.fn(() => 1);
+    const second = jest.fn(() => 2);
 
-  // write tests here
+    ifElse(condition, first, second);
+
+    expect(first).toHaveBeenCalled();
+    expect(second).not.toHaveBeenCalled();
+  });
+
+  it('should ', () => {
+    const condition = () => false;
+    const first = jest.fn(() => 1);
+    const second = jest.fn(() => 2);
+
+    ifElse(condition, first, second);
+
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalled();
+  });
 });
