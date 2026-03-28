8bb2a968cdfdbb727a3c996cf56c408b2b925e1c
Heorhii Savostikov <georgesavostikov@gmail.com>
2026-03-28 22:43:27 +0200

    Added some changes

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -3,25 +3,25 @@
 describe('ifElse', () => {
   const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should call the first callback when the condition is true', () => {
     const condition = () => true;
     const first = jest.fn(() => 1);
     const second = jest.fn(() => 2);
 
     ifElse(condition, first, second);
 
-    expect(first).toHaveBeenCalled();
-    expect(second).not.toHaveBeenCalled();
+    expect(first).toHaveBeenCalledWith();
+    expect(second).not.toHaveBeenCalledWith();
   });
 
-  it('should ', () => {
+  it('should call the second callback when the condition is false', () => {
     const condition = () => false;
     const first = jest.fn(() => 1);
     const second = jest.fn(() => 2);
 
     ifElse(condition, first, second);
 
-    expect(first).not.toHaveBeenCalled();
-    expect(second).toHaveBeenCalled();
+    expect(first).not.toHaveBeenCalledWith();
+    expect(second).toHaveBeenCalledWith();
   });
 });
